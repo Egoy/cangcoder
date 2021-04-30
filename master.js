158d4817d21585de3b2a3a698d209813bed5bdc0
Egoy <fvcangco@gmail.com>
2021-05-01 00:18:14 +0800

    My very first commit for my portfolio

---
+++ master.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById('toggle');
+const navBar = document.getElementById('navbar');
+toggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggle.classList.toggle('active');
+    navBar.classList.toggle('active');
+});
+navBar.addEventListener("click", () => {
+    toggle.classList.toggle('active');
+    navBar.classList.toggle('active');
+  });
